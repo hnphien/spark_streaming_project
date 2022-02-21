ee0033a40c7d9f5d9e4256775c65891039feb900
Huynh Ngoc Phien <51280965+hnphien@users.noreply.github.com>
2022-02-21 16:26:06 +0700

    Update HiveMetastore.scala

--- src/main/scala/Demo/HiveMetastore.scala
+++ src/main/scala/Demo/HiveMetastore.scala
@@ -18,14 +18,6 @@ object HiveMetastore extends App {
   spark.sparkContext.setLogLevel("ERROR")
   println(spark)
 
-//  spark.sql("CREATE DATABASE IF NOT EXISTS kondor")
-//  spark.sql("CREATE DATABASE IF NOT EXISTS dna")
-//  spark.sql("CREATE DATABASE IF NOT EXISTS test")
-
-//  spark.sql("DROP DATABASE IF EXISTS test CASCADE")
-//  spark.sql("DROP DATABASE IF EXISTS dna CASCADE")
-//  spark.sql("DROP DATABASE IF EXISTS kondor CASCADE")
-
   val dfDbs = spark.sql("SHOW DATABASES")
   dfDbs.show()
 
